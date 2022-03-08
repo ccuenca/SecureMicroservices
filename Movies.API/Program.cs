11141cb2a4bf1f1cb6f10ec33f8848f2aeb4c162
Cristhian Cuenca <cristhiancuenca@Cristhians-MacBook-Pro.local>
2022-03-07 23:32:46 -0500

    initial commit

---
+++ Movies.API/Program.cs
@@ -0,0 +1,58 @@
+using Movies.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MoviesContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5005";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+   {
+       options.AddPolicy("ClientIdPolicy", policy => policy.RequireClaim("client_id", "movieClient"));
+   });
+
+var app = builder.Build();
+
+//Seed Database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var moviesContext = services.GetRequiredService<MoviesContext>();
+    MoviesContextSeed.SeedAsync(moviesContext);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Movies.Client/Data/MoviesClientContext.cs
@@ -0,0 +1,17 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.Client.Models;
+
+    public class MoviesClientContext : DbContext
+    {
+        public MoviesClientContext (DbContextOptions<MoviesClientContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.Client.Models.Movie> Movie { get; set; }
+    }
